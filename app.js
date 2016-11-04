4fdc864448808450152aadd717919f24f8f9fb09
ph0gelbice <phogelbice@gmail.com>
2016-11-04 10:53:29 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const cron = require('node-cron');
+const _ = require('underscore');
+const wclurl = "https://www.warcraftlogs.com:443/v1/reports/guild/<guild>/<realm>/<eu/us>?api_key=<key>";
+const discordurl = "https://discordapp.com/api/webhooks/<webhook id>/<webhook token>";
+let temp = [];
+
+let cronJob = cron.schedule('*/15 * * * * *', () => {
+    request.get(wclurl, (err, response, body) => {
+        if (!err && response.statusCode == 200) {
+            update(JSON.parse(body));
+        }
+    })
+});
+
+function update(body) {
+    if (!_.isEqual(temp, body)) {
+        temp = body;
+        request.post(discordurl).form({
+            content: "Found new logs! https://www.warcraftlogs.com/reports/" + _.last(temp).id
+        })
+    }
+}
